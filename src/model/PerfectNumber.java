1b1528701d4767fe77a4ad0a53ce5baa3f838e30
vmlynnj <vmlynnj@gmail.com>
2020-01-22 15:42:46 -0500

    init commit

---
+++ src/model/PerfectNumber.java
@@ -0,0 +1,23 @@
+package model;
+
+public class PerfectNumber {
+	public static final int limit =  1000;
+	
+	public int[] findPerfects() {
+		for(int i = 0; i<limit; i+=2) {
+			
+		}
+		return null;
+	}
+	
+	
+	public boolean isPerfect(int number) {
+		int sum = 1;
+		for (int j=2; j < number; j++ ) {
+			if(j%number == 0) {
+				//sum +=
+			}
+		}
+		return false;
+	}
+}
