a86688742a35e9b3b2c1ca74f509af789e16b147
vmlynnj <vmlynnj@gmail.com>
2020-01-27 13:33:11 -0500

    toyed around

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import model.PerfectNumber;
 import model.PerfectThread;
@@ -8,30 +11,57 @@ import model.PerfectThread;
 public class Main {
 
 	public static void main(String[] args) {
+		Vector<Integer> vector = new Vector<Integer>();
+		PerfectNumber pfn = new PerfectNumber();
+		/*
+		Runnable runny1 = () -> {
+			vector.addAll(pfn.findPerfects(0,5000));
+		};
+		Runnable runny2 = () -> {
+			vector.addAll(pfn.findPerfects(5000,10000));
+		};
+		
+		*/
+		Runnable runny1 = new PerfectThread(000,5000);
+		Runnable runny2 = new PerfectThread(5000,10000);
+		ExecutorService pool = Executors.newFixedThreadPool(2); 
+
+		
+		pool.execute(runny1);
+		pool.execute(runny2);
+		
+		
+		pool.shutdown();
+		/*
+		Thread t = new Thread(runny1);
+		t.start();
+		
 		
 		//int threadAmount = Integer.parseInt(args[0]);
 		//Thread[] pool = new Thread[threadAmount];
+		/*
 		PerfectThread pt1 = new PerfectThread(0,5000);
 		PerfectThread pt2 = new PerfectThread(5000,10000);
 		
 		Thread thread1 = new Thread(pt1);
 		Thread thread2 = new Thread(pt2);
 		
+		
 		thread1.start();
 		thread2.start();
+		
 		try {
-			thread1.join(0);
-			thread2.join(0);
-
-			
+			//thread1.join(0);
+			//thread2.join(0);
+			t.join();
 		} catch (Exception ie) {
 			
 		}
+		*/
 		
+		System.out.println("Size: "+vector);
 		
-		System.out.print("Size: "+PerfectThread.perfects.size());
-		
-		for(int curr: PerfectThread.perfects) {
+		for(int curr: vector) {
 			System.out.println(curr + " ");
 		}
 		/*
--- src/model/PerfectNumber.java
+++ src/model/PerfectNumber.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 
 public class PerfectNumber {
-	public static int upperLimit =1000000;
+	public static int upperLimit =10000;
 	public static int lowerLimit = 0;
 	
 	public PerfectNumber() {
@@ -36,5 +36,25 @@ public class PerfectNumber {
 		return perfects;
 	}
 	
-
+	public ArrayList<Integer> findPerfects(int upper, int lower) {
+		System.out.println("HErE");
+		ArrayList<Integer> perfects = new ArrayList<Integer>();
+		int sum = 1;
+		for(int i = lower; i<upper; i+=2) {
+			for (int j=2; j< i ; j++) {
+				if(i % j == 0 ) {
+					sum +=  j;
+				}
+				if(sum > i) {
+					j = i+1;
+				}
+			}
+			if(sum == i) {
+				perfects.add(i);
+			}
+			sum = 1;
+		}
+		System.out.println("FINISH");
+		return perfects;
+	}
 }
