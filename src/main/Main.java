e6dab8caae6e1701fc55d12cd7de018f56886975
vmlynnj <vmlynnj@gmail.com>
2020-01-30 15:33:35 -0500

    Checked my style

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,76 +1,68 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.Vector;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import model.PerfectNumberFinder;
-import model.PerfectThread;
 
+/**
+ *  The entry point of the application
+ * @author Victoria Jenkins
+ *
+ */
 public class Main {
-	public static final long LIMIT = (long) 30000.0; 
+	public static final long LIMIT = (long) 30000000.0; 
 
+	/**
+	 * The entry point to the application 
+	 * @param args the amount of threads
+	 */
 	public static void main(String[] args) {
 		int threadAmount = Integer.parseInt(args[0]);
 		Thread[] pool = new Thread[threadAmount];
-		Object lock  = new Object();
 		PerfectNumberFinder finder = new PerfectNumberFinder();
 		Vector<Long> perfects = new Vector<Long>();
-		long increment = LIMIT/threadAmount;
-
-		
+		long[] upperBounds = Main.upperBounds(threadAmount);
+		long[] lowerBounds = Main.lowerBounds(threadAmount, upperBounds);
 		long start_time = System.currentTimeMillis();
-		long[] upperBounds = new long[threadAmount];
-		long[] lowerBounds = new long[threadAmount];
-		lowerBounds[0] = 0;
-		upperBounds[0] = increment;
-		for(int k = 1; k<threadAmount; k++) {
-			lowerBounds[k] =upperBounds[k-1];
-			upperBounds[k] = upperBounds[k-1] + increment; 
-		}
-		
-		
-		for(int i=0; i<threadAmount; i++) {
-			Runnable task = () -> {
-				System.out.println("BOUNDS:: Lower: "+ lowerBounds[i] + " Upper: "+ upperBounds[i]);
-				perfects.addAll(finder.findPerfectsTriangles(lowerBounds[i], upperBounds[i]));
-			
-			};
-			Thread thread = new Thread(task);
-			pool[i] = thread;
-			pool[i].start();
-			System.out.println("BOUNDS:: Lower: "+ lowerBounds[i] + " Upper: "+ upperBounds[i]);
-		}
-		
 		try {
-			for(int j = 0; j < threadAmount; j++) {
-				pool[j].join();
+			for (int i = 0; i < threadAmount; i++) {
+				long lower = lowerBounds[i];
+				long upper = upperBounds[i];
+				Runnable task = () -> {
+					perfects.addAll(finder.findPerfects(lower, upper));
+				};
+				Thread thread = new Thread(task);
+				pool[i] = thread;
+				pool[i].start();
+				pool[i].join();
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
-		
         long end_time = System.currentTimeMillis();
-
 		long elapsed_time = (end_time - start_time);
-		
-		System.out.println("Elasped time: "+ elapsed_time + " millis");
-		
-		System.out.println("Size: "+perfects.size());
-		
-		for(long curr: perfects) {
-			System.out.println(curr + " ");
-		}
-		
-		
-		
-		
-		
-		
-		}
-		
-		
+		System.out.println("Elasped time: " + elapsed_time + " millis");
 	}
+	
+	
+	private static long[] upperBounds(int threadAmount) {
+		long increment = LIMIT / threadAmount;
+		long[] upperBounds = new long[threadAmount];
+		upperBounds[0] = increment;
+		for (int k = 1; k < threadAmount; k++) {
+			upperBounds[k] = upperBounds[k - 1] + increment; 
+		}
+		return upperBounds;
+	}
+	
+	private static long[] lowerBounds(int threadAmount, long[] upperBounds) {
+		long[] lowerBounds = new long[threadAmount];
+		lowerBounds[0] = 0;
+		for (int k = 1; k < threadAmount; k++) {
+			lowerBounds[k] = upperBounds[k - 1];
+		}
+		return lowerBounds;
+	}
+}
 
 
--- src/model/PerfectNumberFinder.java
+++ src/model/PerfectNumberFinder.java
@@ -1,112 +1,55 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Vector;
 
+/**
+ * Finds perfect numbers
+ * @author Victoria Jenkins
+ *
+ */
 public class PerfectNumberFinder {
-	private long upperLimit;
-	private long lowerLimit;
 	
-	public PerfectNumberFinder() {
-		this.upperLimit = 10000;
-		this.lowerLimit = 0;
-	}
-	
-	public PerfectNumberFinder(long lowerLimit, long upperLimit) {
-		this.upperLimit = upperLimit;
-		this.lowerLimit = lowerLimit;
-	}
-	
-	public Vector<Long> findPerfects() {
+	/**
+	 *  Finds perfect numbers within the given parameters
+	 * @param lower the lower bounds
+	 * @param upper the upper bounds
+	 * @return a list of perfect numbers within the given range
+	 */
+	public Vector<Long> findPerfects(long lower, long upper) {
 		Vector<Long> perfects = new Vector<Long>();
-		int sum = 1;
-		for(long i = this.lowerLimit; i<this.upperLimit; i+=2) {
-			
-			for (long j=2; j< i ; j++) {
-				if(i % j == 0 ) {
-					sum +=  j;
-				}
-				if(sum > i) {
-					j = i+1;
-				}
+		Vector<Long> triangles = this.findTriangleNumbers(lower, upper);
+		for (Long current : triangles) {
+			if (this.isPerfect(current)) {
+				perfects.add(current);
 			}
-			if(sum == i) {
-				perfects.add(i);
-			}
-			sum = 1;
 		}
 		return perfects;
 	}
 	
-	public synchronized Vector<Long> findPerfectsTriangles(long lower, long upper) {
-		Vector<Long> perfects = new Vector<Long>();
-		long count = 2;
-		long sum =1;
-		long triangle = lower;
-		while(triangle <= upper) {
-			triangle = (count*(count+1))/2;
-
-			for(long i = 2; i<triangle; i++) {
-				if(triangle%i==0) {
-					sum += i;
-				}
-				if(sum >triangle) {
-					i = triangle+1;
-				}
-			}
-			if(sum == triangle) {
-				perfects.add(triangle);
-			}
-			
-			sum = 1;
-			count++;
-					
-		}
-		return perfects;
-	}
 	
-	public ArrayList<Integer> findPerfects(int upper, int lower) {
-		ArrayList<Integer> perfects = new ArrayList<Integer>();
-		int sum = 1;
-		for(int i = lower; i<upper; i+=2) {
-			for (int j=2; j< i ; j++) {
-				if(i % j == 0 ) {
-					sum +=  j;
-				}
-				if(sum > i) {
-					j = i+1;
-				}
-			}
-			if(sum == i) {
-				perfects.add(i);
-			}
-			sum = 1;
-		}
-		System.out.println("FINISH");
-		return perfects;
-	}
-	
-	public Vector<Long> findTriangles() {
+	private Vector<Long> findTriangleNumbers(long lower, long upper) {
 		Vector<Long> triangleNumbers = new Vector<Long>();
 		long count = 2;
-		long triangle = this.lowerLimit;
-		while(triangle <= this.upperLimit) {
-			triangle = (count*(count+1))/2;
+		long triangle = lower;
+		while (triangle <= upper) {
+			triangle = (count * (count + 1)) / 2;
 			triangleNumbers.add(triangle);
 			count++;
 		}
+		triangleNumbers.removeIf(n -> n <= lower);
+		triangleNumbers.removeIf(n -> n >= upper);
 		return triangleNumbers;
 	}
 	
-	public boolean isPerfect(long number) {
-		long sum =1;
+	private boolean isPerfect(long number) {
+		long sum = 1;
 
-		for(long i = 2; i<number; i++) {
-			if(number%i==0) {
+		for (long i = 2; i < number; i++) {
+			if (number % i == 0) {
 				sum += i;
 			}
-			if(sum > number) {
-				i = number+1;
+			if (sum > number) {
+				i = number + 1;
 			}
 		}
 		return (sum == number);
